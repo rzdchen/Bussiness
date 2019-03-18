c457ef0efa197dabb9206ea6a0c555ce09e276b9
chen-pc <rzdchen@outlook.com>
2019-03-18 11:49:07 +0800

    init

---
+++ app/src/main/java/net/cc/business/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package net.cc.business.activity;
+
+import android.os.Bundle;
+
+import net.cc.business.R;
+import net.cc.business.activity.base.BaseActivity;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/net/cc/business/activity/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package net.cc.business.activity.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * created by CC on 2019/3/18.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
